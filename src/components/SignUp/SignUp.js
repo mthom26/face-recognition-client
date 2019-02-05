dd7ab29a55304dd918bd4e71bd62bf7c05e66140
mthom26 <mthom2626@gmail.com>
2019-02-05 23:56:36 +0000

    Add SignUp form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SignUp = () => {
-  return (
-    <div>
-      <h2>Sign Up</h2>
-    </div>
-  );
+const initialFormData = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
 };
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formData: initialFormData,
+      formSuccess: false,
+      formFailure: false,
+      loading: false
+    };
+  }
+
+  onUpdateState = event => {
+    const { target } = event;
+    this.setState({
+      formData: {
+        ...this.state.formData,
+        [target.name]: target.value
+      }
+    });
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    console.log(this.state.formData);
+  };
+
+  render() {
+    const { name, email, password, confirmPassword } = this.state.formData;
+
+    return (
+      <div>
+        <h2>Sign Up</h2>
+        <form>
+          <input
+            onChange={this.onUpdateState}
+            value={name}
+            placeholder="Name"
+            type="text"
+            name="name"
+            id="name"
+          />
+
+          <input
+            onChange={this.onUpdateState}
+            value={email}
+            placeholder="Email"
+            type="text"
+            name="email"
+            id="email"
+          />
+
+          <input
+            onChange={this.onUpdateState}
+            value={password}
+            placeholder="Password"
+            type="password"
+            name="password"
+            id="password"
+          />
+
+          <input
+            onChange={this.onUpdateState}
+            value={confirmPassword}
+            placeholder="Confirm Password"
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+          />
+
+          <button onClick={this.onSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SignUp;

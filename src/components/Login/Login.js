a2f23429f58aca38d6688ee0eaef6ccdb798ca60
mthom26 <mthom2626@gmail.com>
2019-02-06 11:24:33 +0000

    Add Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  );
+const initialFormData = {
+  email: '',
+  password: '',
+  confirmPassword: ''
 };
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formData: initialFormData,
+      formSuccess: false,
+      formFailure: false,
+      loading: false
+    };
+  }
+
+  onUpdateState = event => {
+    const { target } = event;
+    this.setState({
+      formData: {
+        ...this.state.formData,
+        [target.name]: target.value
+      }
+    });
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    try {
+      const url = `${process.env.REACT_APP_SERVER_URL}/login`;
+
+      const result = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(this.state.formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const data = await result.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    const { email, password, confirmPassword } = this.state.formData;
+
+    return (
+      <div>
+        <h2>Login</h2>
+        <form>
+          <input
+            onChange={this.onUpdateState}
+            value={email}
+            placeholder="Email"
+            type="text"
+            name="email"
+            id="email"
+          />
+
+          <input
+            onChange={this.onUpdateState}
+            value={password}
+            placeholder="Password"
+            type="password"
+            name="password"
+            id="password"
+          />
+
+          <input
+            onChange={this.onUpdateState}
+            value={confirmPassword}
+            placeholder="Confirm Password"
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+          />
+
+          <button onClick={this.onSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;

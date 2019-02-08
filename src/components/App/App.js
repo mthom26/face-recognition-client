9ee0aef96b2474fba0fc4bf52f91c84db27d35dc
mthom26 <mthom2626@gmail.com>
2019-02-08 19:08:47 +0000

    Add localStorage
    Add email and json web token to localStorage. On app startup the
    localStorage is checked for token and email and user data is
    fetched.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,11 +20,28 @@ class App extends Component {
 
   setUser = user => {
     this.setState({ user: user });
+    localStorage.setItem('userEmail', user.email);
   };
 
   setToken = token => {
     this.setState({ token: token });
+    localStorage.setItem('token', token);
   };
+
+  getUserData = (token, email) => {
+    // Call server and get User data for provided email
+    console.log('Get User Data');
+  };
+
+  componentDidMount() {
+    // Check localStorage for existing token and user
+    const token = localStorage.getItem('token');
+    const userEmail = localStorage.getItem('userEmail');
+    if (token && userEmail) {
+      this.getUserData(token, userEmail);
+      this.setState({ token: token });
+    }
+  }
 
   render() {
     return (

06dcffa5da16dada9b3c7fd965fefa318b8dcc2a
mthom26 <mthom2626@gmail.com>
2019-02-11 14:27:04 +0000

    Get image
    Get uploaded image from server along with response data from
    Clarifai API (bounding boxes).

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -4,6 +4,8 @@ import XHRUpload from '@uppy/xhr-upload';
 import { DragDrop } from '@uppy/react';
 import '@uppy/drag-drop/dist/style.css';
 
+import styles from './Upload.module.css';
+
 class Upload extends Component {
   constructor(props) {
     super(props);
@@ -23,18 +25,49 @@ class Upload extends Component {
       })
       .on('complete', result => {
         console.log(result);
+        const url = result.successful[0].response.body.imageUrl;
+        const boxes = result.successful[0].response.body.data;
+        this.setImageUrl(url);
+        this.setBoundingBoxes(boxes);
+        // uppy.reset() called to reset this uppy instance and allow more
+        // uploads without refreshing
+        this.uppy.reset();
       });
+
+    this.state = {
+      showImage: false,
+      imageUrl: '',
+      boundingBoxes: null
+    };
   }
+
+  setImageUrl = url => {
+    this.setState({ imageUrl: url, showImage: true });
+  };
+
+  setBoundingBoxes = boxes => {
+    this.setState({ boundingBoxes: boxes });
+  };
 
   componentWillUnmount() {
     this.uppy.close();
   }
 
   render() {
+    const { showImage, imageUrl } = this.state;
+
     return (
       <div>
         <DragDrop uppy={this.uppy} />
         <button onClick={() => this.uppy.upload()}>Upload!</button>
+        {showImage && (
+          <div className={styles.imageContainer}>
+            <img
+              src={`${process.env.REACT_APP_SERVER_URL}/${imageUrl}`}
+              alt=""
+            />
+          </div>
+        )}
       </div>
     );
   }

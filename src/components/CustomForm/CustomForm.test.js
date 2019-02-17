cc0d72ee24dac01b0a7e15ce70c6f4fc58b13c63
mthom26 <mthom2626@gmail.com>
2019-02-17 21:27:56 +0000

    Add interaction tests for CustomForm

--- src/components/CustomForm/CustomForm.test.js
+++ src/components/CustomForm/CustomForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import '../../setupEnzyme';
 
 import CustomForm from './CustomForm';
@@ -25,6 +25,62 @@ describe('CustomForm', () => {
       };
       const wrapper = shallow(<CustomForm {...props} />);
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('handles user interaction', () => {
+    it('should call onSubmitAction when clicked', () => {
+      const props = {
+        showName: true,
+        showEmail: true,
+        showPassword: true,
+        showPwConfirm: true,
+        onSubmitAction: jest.fn()
+      };
+      const wrapper = mount(<CustomForm {...props} />);
+      wrapper.find('button').simulate('click');
+      expect(props.onSubmitAction).toHaveBeenCalledTimes(1);
+      expect(props.onSubmitAction).toHaveBeenCalledWith({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      });
+    });
+    it('should register data on input fields', () => {
+      const props = {
+        showName: true,
+        showEmail: true,
+        showPassword: true,
+        showPwConfirm: true,
+        onSubmitAction: jest.fn()
+      };
+      const wrapper = mount(<CustomForm {...props} />);
+
+      const nameInput = wrapper.find('#name');
+      nameInput.instance().value = 'testName';
+      nameInput.simulate('change');
+
+      const emailInput = wrapper.find('#email');
+      emailInput.instance().value = 'testemail@mail.com';
+      emailInput.simulate('change');
+
+      const pwInput = wrapper.find('#password');
+      pwInput.instance().value = 'muhpassword';
+      pwInput.simulate('change');
+
+      const pwcInput = wrapper.find('#confirmPassword');
+      pwcInput.instance().value = 'muhpassword';
+      pwcInput.simulate('change');
+
+      wrapper.find('button').simulate('click');
+      expect(props.onSubmitAction).toHaveBeenCalledTimes(1);
+      expect(props.onSubmitAction).toHaveBeenCalledWith({
+        name: 'testName',
+        email: 'testemail@mail.com',
+        password: 'muhpassword',
+        confirmPassword: 'muhpassword'
+      });
     });
   });
 });

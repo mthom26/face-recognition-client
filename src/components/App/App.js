16df90e4f92a6aff9be9af8e6e7a53e58d157dd0
mthom26 <mthom2626@gmail.com>
2019-02-21 03:11:29 +0000

    Add rerouting to login/signup paths

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -95,24 +95,32 @@ class App extends Component {
             <Route
               exact
               path="/login"
-              render={() => (
-                <Login
-                  setUser={this.setUser}
-                  setToken={this.setToken}
-                  setIsLoggedIn={this.setIsLoggedIn}
-                />
-              )}
+              render={() =>
+                isLoggedIn ? (
+                  <Upload token={this.state.token} />
+                ) : (
+                  <Login
+                    setUser={this.setUser}
+                    setToken={this.setToken}
+                    setIsLoggedIn={this.setIsLoggedIn}
+                  />
+                )
+              }
             />
             <Route
               exact
               path="/signup"
-              render={() => (
-                <SignUp
-                  setUser={this.setUser}
-                  setToken={this.setToken}
-                  setIsLoggedIn={this.setIsLoggedIn}
-                />
-              )}
+              render={() =>
+                isLoggedIn ? (
+                  <Upload token={this.state.token} />
+                ) : (
+                  <SignUp
+                    setUser={this.setUser}
+                    setToken={this.setToken}
+                    setIsLoggedIn={this.setIsLoggedIn}
+                  />
+                )
+              }
             />
             <Route
               exact
